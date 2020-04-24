341f8c71ef614258db3989f6241e5541fe5d4692
MOATASSEM BILLAH SINDHU <DEV-MOATASSEM@users.noreply.github.com>
2020-04-25 02:37:23 +0500

    Initial

---
+++ src/ASSCII.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOATASSEM SINDHU
+ */
+public class ASSCII {
+        public static void main(String []args){
+        char z='Z';
+        System.out.println((int)z);
+        //vice versa
+        
+        int w=65;
+        System.out.println((char)w);
+        
+    }
+    
+}
